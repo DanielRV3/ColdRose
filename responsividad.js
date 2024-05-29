a58b2a5c4bda49cf24c0e61cd37ee3a8d0c0fbf2
Daniel Ramirez <183682@upslp.edu.mx>
2024-05-29 13:53:35 -0600

    formato general y links

--- responsividad.js
+++ responsividad.js
@@ -1,4 +1,5 @@
-const col_aux = document.getElementsByClassName("col-aux")
+const col_aux = document.getElementsByClassName("col-aux");
+
 function navegador(){
     let width = document.body.offsetWidth
     if (width>=768){
@@ -11,9 +12,10 @@ function navegador(){
         for(let i=0;i<col_aux.length;i++) {
             col_aux[i].style.display = "flex";
         }
+        document.getElementsByTagName("main")[0].style.scrollPaddingTop = "0";
     }else{
         document.getElementById("nav-pc").style.display = "none";
-        document.getElementById("nav-cel").style.display = "flex";
+        document.getElementById("nav-cel").style.display = "block";
 
         document.getElementById("fondo-pc").style.display = "none";
         document.getElementById("fondo-cel").style.display = "flex";
@@ -21,6 +23,7 @@ function navegador(){
         for(let i=0;i<col_aux.length;i++) {
             col_aux[i].style.display = "none";
         }
+        document.getElementsByTagName("main")[0].style.scrollPaddingTop = "55px";
     }
 }
 
@@ -32,5 +35,11 @@ function desplegar() {
         bloques.style.display = "block";
     }
 }
+
+function copiar(){
+    navigator.clipboard.writeText("https://danielrv3.github.io/ColdRose/");
+}
+
+
 
 window.addEventListener("resize",navegador);
