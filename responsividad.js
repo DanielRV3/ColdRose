9ccf8d12878a1904dabf5a55a813d15d4dd74369
Daniel Ramirez <183682@upslp.edu.mx>
2024-05-29 18:18:14 -0600

    arreglo scroll

--- responsividad.js
+++ responsividad.js
@@ -1,8 +1,9 @@
 const col_aux = document.getElementsByClassName("col-aux");
+const secciones = document.getElementsByTagName("section")
 
 function navegador(){
     let width = document.body.offsetWidth
-    if (width>=768){
+    if (width>=768){    //diseño de escritorio
         document.getElementById("nav-pc").style.display = "flex";
         document.getElementById("nav-cel").style.display = "none";
 
@@ -12,8 +13,11 @@ function navegador(){
         for(let i=0;i<col_aux.length;i++) {
             col_aux[i].style.display = "flex";
         }
+        for(let i=0;i<secciones.length;i++){
+            secciones[i].getElementsByTagName("h2")[0].style.scrollSnapAlign = "start";
+        }
         document.getElementsByTagName("main")[0].style.scrollPaddingTop = "0";
-    }else{
+    }else{          //diseño de celular
         document.getElementById("nav-pc").style.display = "none";
         document.getElementById("nav-cel").style.display = "block";
 
@@ -23,10 +27,12 @@ function navegador(){
         for(let i=0;i<col_aux.length;i++) {
             col_aux[i].style.display = "none";
         }
+        for(let i=0;i<secciones.length;i++){
+            secciones[i].getElementsByTagName("h2")[0].style.scrollSnapAlign = "none";
+        }
         document.getElementsByTagName("main")[0].style.scrollPaddingTop = "55px";
     }
 }
-
 function desplegar() {
     let bloques = document.getElementById("myLinks");
     if (bloques.style.display === "block") {
@@ -35,11 +41,9 @@ function desplegar() {
         bloques.style.display = "block";
     }
 }
-
 function copiar(){
     navigator.clipboard.writeText("https://danielrv3.github.io/ColdRose/");
 }
 
-
-
 window.addEventListener("resize",navegador);
+
